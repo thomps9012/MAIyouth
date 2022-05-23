db56c0ff277bd8b3480729dd25b66e6cbddc0aba
Samuel Thompson <thomps9012@gmail.com>
2022-05-23 10:16:12 -0400

    added in title case functionality

---
+++ utils/titleCase.tsx
@@ -0,0 +1,5 @@
+export default function titleCase(str: string) {
+    return str.replace(/\w\S*/g, (txt) => {
+        return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase();
+    });
+}
